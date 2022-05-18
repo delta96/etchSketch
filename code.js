e88de34f9bbb6948ff3cc61061fd1469e5683a96
Ricardo Del Valle <rickydelvalle96@gmail.com>
2022-05-18 09:20:59 -0400

    Add buttons for color options

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 const squares = document.querySelectorAll('.pixel');
 const selectPaint = document.querySelector('#start');
 const selectReset = document.querySelector('#reset');
+const selectBlue = document.querySelector('#blue');
+const selectRed = document.querySelector('#red');
 
 
     function paint(){
@@ -15,9 +17,27 @@ const selectReset = document.querySelector('#reset');
     function reset(){
         squares.forEach (square => {
 
-            square.style.backgroundColor = 'aqua';
+            square.style.backgroundColor = 'white';
         })
+    }
+
+    function blue(){
+        squares.forEach (square => {
+
+            square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = 'blue';
+        })});
+    }
+
+    function red(){
+        squares.forEach (square => {
+
+            square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = 'red';
+        })});
     }
 
 selectPaint.addEventListener('click', paint);
 selectReset.addEventListener('click', reset);
+selectBlue.addEventListener('click', blue);
+selectRed.addEventListener('click', red);
