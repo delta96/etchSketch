dfa6cf068f2054eef2ea07dee818e916f3074215
Ricardo Del Valle <rickydelvalle96@gmail.com>
2022-05-18 10:34:24 -0400

    Create a function to initialize a 16x16 grid for sketch pad

--- code.js
+++ code.js
@@ -1,11 +1,22 @@
-const squares = document.querySelectorAll('.pixel');
+
 const selectPaint = document.querySelector('#start');
 const selectReset = document.querySelector('#reset');
 const selectBlue = document.querySelector('#blue');
 const selectRed = document.querySelector('#red');
+const sketchPad = document.querySelector('#grid');
+const selectCreate = document.querySelector('#create');
+
+function addPixels(){
+    for(i=0; i < 256; i++){
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        document.getElementById('grid').appendChild(pixel);
+    }
+}
 
 
     function paint(){
+       const squares = document.querySelectorAll('.pixel');
         squares.forEach (square => {
     
             square.addEventListener('mouseover', function(){
@@ -15,6 +26,7 @@ const selectRed = document.querySelector('#red');
     }
 
     function reset(){
+       const squares = document.querySelectorAll('.pixel');
         squares.forEach (square => {
 
             square.style.backgroundColor = 'white';
@@ -22,6 +34,7 @@ const selectRed = document.querySelector('#red');
     }
 
     function blue(){
+       const squares = document.querySelectorAll('.pixel');
         squares.forEach (square => {
 
             square.addEventListener('mouseover', function(){
@@ -30,6 +43,7 @@ const selectRed = document.querySelector('#red');
     }
 
     function red(){
+        const squares = document.querySelectorAll('.pixel');
         squares.forEach (square => {
 
             square.addEventListener('mouseover', function(){
@@ -37,6 +51,9 @@ const selectRed = document.querySelector('#red');
         })});
     }
 
+    
+
+selectCreate.addEventListener('click', addPixels);
 selectPaint.addEventListener('click', paint);
 selectReset.addEventListener('click', reset);
 selectBlue.addEventListener('click', blue);
